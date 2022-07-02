cc67a6b45ff55625dbd66575c932b7d4d3aac468
melihozmen9 <105754659+melihozmen9@users.noreply.github.com>
2022-07-02 19:10:41 +0300

    optional

--- optional/ViewController.swift
+++ optional/ViewController.swift
@@ -12,6 +12,26 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        var benimadim = "melih"
+        var benimikinciadim: String? = "ahmet"// soru işareti koyarak aslında buranın boş gelebileceğini belirtmiş olduk
+        
+        var benimsoyadim = "ozmen"
+        
+        // değerleri sil
+       // benimadim = nil
+        //benimsoyadim = nil
+        //benimikinciadim = nil
+        
+        print("adim:\(benimadim)")
+        print("adim:\(benimikinciadim)")
+        print("soyadim:\(benimsoyadim)")
+        
+        if let ikinciadim = benimikinciadim{
+            print("adim:\(benimadim)")
+            print("adim:\(ikinciadim)")
+            print("soyadim:\(benimsoyadim)")
+        }
+        // ! işareti ile force unwrapping yapılır.
     }
 
 
